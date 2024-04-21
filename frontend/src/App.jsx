db7bcf72e299652443267f9ac4c8a84f8584fd19
mukies <muk.yess@gmai.com>
2024-04-21 17:33:31 +0545

    initial

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="text-red-600 text-3xl flex flex-col">
+      hello world
+      <button className="btn btn-primary btn-lg max-w-max">hello</button>
+    </div>
+  );
+}
+
+export default App;
